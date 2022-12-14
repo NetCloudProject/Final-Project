1c12fad10fe991c41e6f68d8df339f5a6e2a061c
Coulson Zhang <zheyu3@illinois.edu>
2022-12-14 16:36:35 -0500

    backend handle post/delete/get request

--- Back/Shopping-list/main_application.py
+++ Back/Shopping-list/main_application.py
@@ -28,7 +28,7 @@ def get_health():
 
 
 @app.route("/api/summary", methods=["GET"])
-def get_student_by_uni():
+def showSummary():
 
     result = Shop.get_by_key(None)
 
@@ -44,13 +44,82 @@ def get_all_items():
     result = Shop.get_by_key(None)
 
     if result:
-        # rsp = Response(json.dumps(result), status=200, content_type="application.json")
         rsp = jsonify({"response": result})
-        # rsp[item_list] = 1
     else:
         rsp = Response("NOT FOUND", status=404, content_type="text/plain")
 
     return rsp
+
+@app.route("/show_list", methods=["GET"])
+def showList():
+    result = Shop.get_list()
+
+    if result:
+        rsp = jsonify({"response": result})
+    else:
+        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+
+    return rsp
+
+
+@app.route("/show_product/<id>", methods=["GET"])
+def showProduct(id):
+    result = Shop.get_product(id)
+
+    if result:
+        rsp = jsonify({"response": result})
+    else:
+        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
+
+    return rsp
+
+
+
+""" 
+Post body format:
+        {
+            "id": 3,
+            "name": "Sample"
+        }
+POST request add new product
+DELTE request delete existing product
+"""
+@app.route("/product", methods=["POST", "DELETE"])
+def product_request():
+        req_data = request.get_json()
+        id = req_data['id']
+        product = req_data['name']
+        if request.method == "POST":
+            Shop.add_product(product, id)
+        if request.method == "DELETE":
+            Shop.delete_product(product, id)
+        return "I have your post request"
+
+""" 
+{
+	"id": 5,
+	"address": "Wesasdasdasdasday",
+    "date": "2022-11-16"
+}
+POST request add new list
+DELTE request delete existing list
+GET request update date of existing list
+"""
+
+@app.route("/list", methods=["POST", "DELETE", "GET"])
+def list_request():
+        req_data = request.get_json()
+        id = req_data['id']
+        address = req_data['address']
+        date = req_data['date']
+        print("Request have id:{}, and the product:{}".format(id, address))
+        if request.method == "POST":
+            Shop.add_list(id, address, date)
+        if request.method == "DELETE":
+            Shop.delete_list(id, address, date)
+        if request.method == "GET":
+            Shop.update_list(id, date)
+        return "I have your post request"
 
 
 if __name__ == "__main__":
--- Back/Shopping-list/shopping_details.py
+++ Back/Shopping-list/shopping_details.py
@@ -36,3 +36,72 @@ class Shop:
         result = cur.fetchone()
 
         return result
+
+    @staticmethod
+    def get_list():
+        sql = "SELECT list.list_id, list.shopping_location, list.shopping_date FROM shopping_list.list";
+        conn = Shop._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=None)
+        result = cur.fetchall()
+        return result
+
+    @staticmethod
+    def get_product(key):
+        sql = "SELECT product.product_name FROM shopping_list.product where list_id = %s";
+        conn = Shop._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=key)
+        result = cur.fetchall()
+        return result
+
+    @staticmethod
+    def add_product(name, id):
+        sql = "INSERT INTO shopping_list.product VALUES (%s, %s)";
+        conn = Shop._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=(name, id))
+        result = cur.fetchall()
+        return result
+
+    @staticmethod
+    def delete_product(name, id):
+        sql = "DELETE FROM shopping_list.product WHERE product_name = %s and list_id = %s";
+        conn = Shop._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=(name, id))
+        result = cur.fetchall()
+        return result
+
+    @staticmethod
+    def add_list(id, address, date):
+        sql = "INSERT INTO shopping_list.list VALUES (%s, %s, %s)";
+        conn = Shop._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=(id, address, date))
+        result = cur.fetchall()
+        return result
+
+    @staticmethod
+    def delete_list(id, address, date):
+        sql = "DELETE FROM shopping_list.list WHERE list_id = %s and shopping_location = %s and shopping_date = %s";
+        conn = Shop._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=(id, address, date))
+        result = cur.fetchall()
+        return result
+
+    @staticmethod
+    def update_list(id, date):
+        sql = "UPDATE shopping_list.list SET shopping_date = %s WHERE list_id = %s";
+        conn = Shop._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=(date, id))
+        result = cur.fetchall()
+        return result
+
+
+
+
+
+
